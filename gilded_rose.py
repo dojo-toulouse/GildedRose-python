0b052d908850c79bd1801307fd74e1b5b1bf8c53
pydev <apieum@gmail.com>
2013-06-25 19:35:32 +0200

    @Antoine extract method decrease quality

--- gilded_rose.py
+++ gilded_rose.py
@@ -6,12 +6,16 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def decrease_quality(self, item):
+        if item.quality > 0:
+                item.quality = item.quality - 1
+
     def update_quality(self):
         for item in self.items:
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                self.decrease_quality(item)
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -22,14 +26,13 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            
+            item.sell_in = item.sell_in - 1
+            
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                        self.decrease_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
